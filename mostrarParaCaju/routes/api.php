468d249f65c03aacd18e5f7a833fbb171fadb1ca
Vitor Azevedo <vitor.azevedo009@gmail.com>
2018-04-25 09:44:15 -0300

    Created Model -a

--- mostrarParaCaju/routes/api.php
+++ mostrarParaCaju/routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/produtos','ProdutoController');
+
+Route::group(['prefix'=>'produtos'],function(){
+  Route::apiResource('/{produtos}/reviews','ReviewController');
+});
